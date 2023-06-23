c8c147082a842c52fc13b24fcc15f81a2704269e
Md Mehrab Hossain <mehrab.it99@gmail.com>
2023-06-23 23:19:13 +0300

    JS-init

---
+++ index.js
@@ -0,0 +1,48 @@
+// Getting started 
+console.log("Md Mehrab Hossain");
+
+// Variables
+var nameMine = "Mehrab";
+let My_Age = 50;
+const Your_Adress = "Finland";
+console.log(nameMine, My_Age, Your_Adress);
+
+// Primitive types
+///Data types
+ let nameFirst = "TT";
+ let valueF1 = true;
+ let myAge = 20;
+ let dataTy1 = undefined;
+ let dataTy2 = null;
+
+ console.log ( typeof nameFirst);
+ console.log ( typeof valueF1);
+ console.log ( typeof myAge);
+ console.log ( typeof dataTy1);
+ console.log ( typeof dataTy2);
+
+ /// JS Objects
+ let bioPerson = {
+   namePerson : "Hossain",
+   agePerson : 31,
+   adressPerson : "Finland"
+ };
+////Dot notation 
+ console.log(bioPerson);
+ console.log(bioPerson.agePerson);
+
+ ////Bracket notation 
+ console.log(bioPerson ["namePerson"]);
+
+
+ ///JS Array
+ let nameList = ["Navi", "Shaki", "Robin"];
+ let bioPerson2 = {
+  namePerson : ["Navi", "Shaki", "Robin"],
+  agePerson : [31,32, 45],
+  adressPerson : ["Finland", "Bangladesh", "Istonia"],
+};
+ console.log(bioPerson2);
+
+
+ //JS Function
