46a625cc869905fa4fd19f6ce66896c1b1aaad51
Md Mehrab Hossain <mehrab.it99@gmail.com>
2023-08-01 20:51:06 +0300

    function, export, import

--- index2.js
+++ index2.js
@@ -15,3 +15,45 @@ for(let l_scope=1; l_scope<5; l_scope++)
     console.log(l_scope);
 }
 console.log(l_scope);
+
+//Declearation same veriable in Locally and Globally
+
+for(let g_localGlobal=1; g_localGlobal<5; g_localGlobal++)
+{
+    console.log(g_localGlobal);
+}
+var g_localGlobal="Mehrab"; 
+
+//Constant-> We can change/add property and value for it 
+const objects1 = {nam:"Mehrab"};
+objects1.nam = "Hossain"
+objects1.age = 29;
+
+
+//Function--> Declaration Vs Expression (Arrow function)
+///Function--> Declaration
+function callNumber (){
+    console.log("Mehrab");
+}
+
+//Function--> Expression
+var callNumber1 = function () {
+    console.log("Hossain");
+}
+
+//Function--> Arrow
+var callNumber2 = () => {
+    console.log("Sagor");
+}
+
+
+//Export with Default Vs Without Default 
+///Export with Default
+
+export default callNumber;
+export {callNumber1, callNumber2 }; 
+
+///Export without Default
+export var callNumber3 = () => {
+    console.log("Sagor");
+}
---
+++ index3.js
@@ -0,0 +1,8 @@
+//Import with Default Vs Without Default 
+///Import with Default
+import callNumber2 from "./index2";
+console.log("Call Number-1:", callNumber2);
+
+///Import without Default
+import {callNumber3} from "./index2";
+console.log("Call Number-2:", callNumber3); 
